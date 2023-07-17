1e3cef70c23ba0e606fd9bc3b550f98eff83150b
soniabergamini <sounia96@outlook.it>
2023-07-17 17:19:12 +0200

    feat: add card component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,13 @@
 <script>
 import axios from 'axios'
+import MainProjectCard from './MainProjectCard.vue';
 
 
 export default {
     name: "AppMain",
+    components: {
+        MainProjectCard
+    },
     data() {
         return {
             baseAPIurl: "http://localhost:8000/api/",
@@ -18,7 +22,7 @@ export default {
         getProjectData() {
             this.loading = true
             axios.get(this.baseAPIurl + this.projAPIpath).then((response) => {
-                console.log(response.data.results),
+                // console.log(response.data.results),
                 this.loading = false,
                 this.projects = response.data.results
             }).catch(err=> {
@@ -36,17 +40,7 @@ export default {
 
 <template>
     <section class="flex justify-between flex-wrap gap-2 p-6">
-        <div v-for="project in projects" class="border w-[calc(100%/5-8px)] rounded text-center flex flex-col justify-between">
-            <img :src='"http://localhost:8000/storage/" + project.image' :alt="project.name" class="w-full max-h-72 object-center object-cover rounded">
-            <div class="flex flex-col gap-2 justify-between">
-                <h2 class="font-bold text-lg text-primary-blu uppercase tracking-wider">{{ project.name }}</h2>
-                <div class="px-3 text-xs text-left">
-                    <p>{{ project.description }}</p>
-                </div>
-                <p class="text-sm italic text-gray-500 my-2">({{ project.stack }})</p>
-            </div>
-            <a :href="project.link" class="font-bold border tracking-wider bg-primary-blu text-white hover:border-spacing-2 border-primary-blu hover:bg-white hover:text-primary-blu">VISIT</a>
-        </div>
+        <MainProjectCard :projects="projects" />
     </section>
 </template>
 
---
+++ src/components/MainProjectCard.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from 'axios'
+
+
+export default {
+    name: "AppMain",
+    props: {
+        projects: Array
+    }
+}
+
+</script>
+
+<template>
+    <div v-for="project in projects" class="border w-[calc(100%/5-8px)] rounded text-center flex flex-col justify-between">
+        <img :src='"http://localhost:8000/storage/" + project.image' :alt="project.name" class="w-full max-h-72 object-center object-cover rounded">
+        <div class="flex flex-col gap-2 justify-between">
+            <h2 class="font-bold text-lg text-primary-blu uppercase tracking-wider">{{ project.name }}</h2>
+            <div class="px-3 text-xs text-left">
+                <p>{{ project.description }}</p>
+            </div>
+            <p class="text-sm italic text-gray-500 my-2">({{ project.stack }})</p>
+        </div>
+        <a :href="project.link" class="font-bold border tracking-wider bg-primary-blu text-white hover:border-spacing-2 border-primary-blu hover:bg-white hover:text-primary-blu">VISIT</a>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../variables.scss' as *;
+</style>
