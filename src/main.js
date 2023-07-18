ca5886cec38f802d4869025e8c49841a5f616ecf
soniabergamini <sounia96@outlook.it>
2023-07-18 16:34:22 +0200

    feat: add homepage vuetify carousel

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ import './variables.scss';
 import {router} from './router';
 import App from './App.vue';
 
-createApp(App).use(router).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(router).use(vuetify).mount('#app')
--- src/pages/AppHomepage.vue
+++ src/pages/AppHomepage.vue
@@ -2,50 +2,32 @@
 // import axios from 'axios'
 // import MainProjectCard from './MainProjectCard.vue';
 
-
-export default {
+  export default {
     name: "AppHomepage",
-    // components: {
-    //     MainProjectCard
-    // },
-    // data() {
-    //     return {
-    //         baseAPIurl: "http://localhost:8000/api/",
-    //         projAPIpath: "projects",
-    //         projects: [],
-    //         loading: false,
-    //         loadingError: false,
-    //         projTotalPages: 0,
-    //         projCurrentPage: 0
-
-    //     }
-    // },
-    // methods: {
-    //     getProjectData(pageNumber) {
-    //         this.loading = true
-    //         axios.get(this.baseAPIurl + this.projAPIpath, {params: {page: pageNumber}}).then((response) => {
-    //             console.log(response.data.results),
-    //             this.loading = false,
-    //             this.projects = response.data.results.data,
-    //             this.projCurrentPage = response.data.results.current_page,
-    //             this.projTotalPages = response.data.results.last_page
-    //         }).catch(err=> {
-    //             this.loading = false,
-    //             this.loadingError = "Cannot load projects data. " + err
-    //         })
-    //     }
-    // },
-    // mounted() {
-    //     this.getProjectData(1)
-    // }
-}
-
+    data () {
+      return {
+        items: [
+          { src: 'https://picsum.photos/id/923/900/600', },
+          { src: 'https://picsum.photos/id/433/900/600',},
+          { src: 'https://picsum.photos/id/659/900/600',},
+          { src: 'https://picsum.photos/id/407/900/600',},
+        ],
+      }
+    },
+  }
 </script>
 
 <template>
+    <v-carousel cycle show-arrows="hover" height="700" hide-delimiters>
+        <template v-slot:prev="{ props }">
+          <v-btn variant="tonal" color="light" @click="props.onClick"><i class="fa-solid fa-chevron-left text-white fa-xl"></i></v-btn>
+        </template>
+        <template v-slot:next="{ props }">
+          <v-btn variant="tonal" color="light" @click="props.onClick"><i class="fa-solid fa-chevron-right text-white fa-xl"></i></v-btn>
+        </template>
+        <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" cover></v-carousel-item>
+    </v-carousel>
 
-   <h3>CIAO IO SONO HOMEPAGE</h3>
-   
 </template>
     
 
