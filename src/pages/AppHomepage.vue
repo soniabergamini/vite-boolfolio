132620372d36b49249335039a4218f1fe949d91a
soniabergamini <sounia96@outlook.it>
2023-07-19 19:00:28 +0200

    style(AppHomepage): improve carousel, add button

--- src/pages/AppHomepage.vue
+++ src/pages/AppHomepage.vue
@@ -16,6 +16,9 @@
 </script>
 
 <template>
+  <section>
+    
+    <!-- Carousel -->
     <v-carousel cycle show-arrows="hover" height="700" hide-delimiters>
         <template v-slot:prev="{ props }">
           <v-btn variant="tonal" color="light" @click="props.onClick"><i class="fa-solid fa-chevron-left text-white fa-xl"></i></v-btn>
@@ -25,6 +28,13 @@
         </template>
         <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" cover></v-carousel-item>
     </v-carousel>
+
+    <!-- Button Redirect On Cick to Route 'projects' -->
+    <router-link :to="{name: 'projects'}" class="absolute z-10 top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%]">
+      <v-btn variant="flat" density="default" size="x-large" rounded="xs" class="hover:text-primary-blu font-bold px-3 py-1 bg-primary-blu hover:bg-white text-slate-50	">ALL PROJECTS</v-btn>
+    </router-link>
+
+  </section>  
 </template>
     
 
