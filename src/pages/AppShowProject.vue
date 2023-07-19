cb04f8313dbd1073c04f648cfe022a575c509d7d
soniabergamini <sounia96@outlook.it>
2023-07-19 18:24:42 +0200

    style(AppShowProject): improve project card

--- src/pages/AppShowProject.vue
+++ src/pages/AppShowProject.vue
@@ -34,23 +34,38 @@ import axios from 'axios';
 
 <template>
     <section v-if="project" class="h-full flex justify-center items-start py-5">
-        <div class="border w-[calc(100%/3-8px)] rounded text-center flex flex-col justify-between">
+        <div class="border w-2/3 rounded flex flex-wrap items-center justify-between">
+
+            <h2 class="w-full font-bold text-3xl text-primary-blu uppercase tracking-wider text-center py-5">{{ project.name }}</h2>
         
-            <img :src='"http://localhost:8000/storage/" + project.image' :alt="project.name" class="w-full max-h-80 object-center object-cover rounded">
+            <img :src='"http://localhost:8000/storage/" + project.image' :alt="project.name" class="w-1/2 max-h-96 object-center object-cover px-5">
         
             <!-- Project Info -->
-            <div class="flex flex-col gap-2 justify-between">
-                <h2 class="font-bold text-lg text-primary-blu uppercase tracking-wider">{{ project.name }}</h2>
+            <div class="flex flex-col gap-y-2 justify-between w-1/2 p-5 border-l">
+                <p>
+                    <strong>Domain: </strong>
+                    <span>{{ project.domain }}</span>
+                </p>
                 <p>
                     <strong>Type: </strong>
                     <span>{{ project.type ? project.type.name : "N/A"}}</span>
                 </p>
-                <div class="px-3 text-xs text-left">
-                    <p>{{ project.description }}</p>
-                </div>
-                <p class="text-sm italic text-gray-500 my-2">({{ project.stack }})</p>
+                <p>
+                    <strong>Technologies: </strong>
+                    <template v-for="item in project.technologies">
+                        <span>{{ item.name}}&nbsp </span>
+                    </template>
+                </p>
+                <p>
+                    <strong>Project Abstract: </strong>
+                    <span>{{ project.description }}</span>
+                </p>
+                <p>
+                    <strong>Release Date: </strong>
+                    <span>{{ project.date.slice(0, 10) }}</span>
+                </p>
             </div>
-            <a :href="project.link" class="font-bold border tracking-wider bg-primary-blu text-white hover:border-spacing-2 border-primary-blu hover:bg-white hover:text-primary-blu py-1">VISIT</a>
+            <a :href="project.link" class="font-bold border tracking-wider bg-primary-blu hover:border-spacing-2 border-primary-blu hover:bg-white hover:text-primary-blu py-1 w-full text-center mt-5">VISIT</a>
         </div>
     </section>
     
